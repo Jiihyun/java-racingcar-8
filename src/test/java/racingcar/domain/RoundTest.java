1f099098d9e8a365c8785c5303918762a738aad4
Jiihyun <cwlgusc@gmail.com>
2025-10-26 00:17:03 +0900

    test: 라운드 감소 테스트 추가

--- src/test/java/racingcar/domain/RoundTest.java
+++ src/test/java/racingcar/domain/RoundTest.java
@@ -1,8 +1,11 @@
 package racingcar.domain;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import racingcar.exception.ExceptionMessage;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,5 +29,25 @@ class RoundTest {
         assertThatThrownBy(() -> new Round(round))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage(ExceptionMessage.ROUND_OUT_OF_RANGE.getMessage());
+    }
+
+    @Test
+    void 라운드를_감소할_수_있다() {
+        // given
+        Round round = new Round(2);
+        // when
+        Optional<Round> decreasedRound = round.decrease();
+        // then
+        assertThat(decreasedRound).get().extracting(Round::value).isEqualTo(1);
+    }
+
+    @Test
+    void 라운드가_종료되면_빈_값을_반환한다() {
+        // given
+        Round round = new Round(1);
+        // when
+        Optional<Round> decreasedRound = round.decrease();
+        // then
+        assertThat(decreasedRound).isEmpty();
     }
 }
