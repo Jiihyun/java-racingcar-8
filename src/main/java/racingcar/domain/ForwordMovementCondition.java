61a1fbcea27739fb435133c6a908d4cef38a9633
Jiihyun <cwlgusc@gmail.com>
2025-10-26 00:04:46 +0900

    test: 전진 조건 판단 테스트 추가

--- src/main/java/racingcar/domain/ForwordMovementCondition.java
+++ src/main/java/racingcar/domain/ForwordMovementCondition.java
@@ -2,7 +2,7 @@ package racingcar.domain;
 
 public class ForwordMovementCondition implements MovementCondition {
 
-    public static final int THRESHOLD = 4;
+    private static final int THRESHOLD = 4;
 
     @Override
     public boolean isSatisfiedBy(int number) {
---
+++ src/test/java/racingcar/domain/ForwordMovementConditionTest.java
@@ -0,0 +1,23 @@
+package racingcar.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ForwordMovementConditionTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "4, TRUE",
+            "3, FALSE"
+    })
+    void 전진_조건을_만족하는지_판단할_수_있다(int number, boolean expectedResult) {
+        // given
+        ForwordMovementCondition movementCondition = new ForwordMovementCondition();
+        // when
+        boolean result = movementCondition.isSatisfiedBy(number);
+        // then
+        assertThat(result).isEqualTo(expectedResult);
+    }
+}
