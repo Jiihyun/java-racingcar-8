029dab440019039b32fd0c9534ce0ecebb955483
Jiihyun <cwlgusc@gmail.com>
2025-10-27 02:08:58 +0900

    refactor: 속도 빠른 자동차들 구하는 메서드 이름 변경

--- src/main/java/racingcar/domain/Referee.java
+++ src/main/java/racingcar/domain/Referee.java
@@ -7,13 +7,13 @@ import java.util.List;
 public class Referee {
 
     public Winners judge(Cars cars) {
-        List<Car> winners = getResult(cars);
+        List<Car> winners = findFastestCars(cars);
         return new Winners(winners.stream()
                 .map(Car::getName)
                 .toList());
     }
 
-    private List<Car> getResult(Cars cars) {
+    private List<Car> findFastestCars(Cars cars) {
         Car fastestCar = cars.getFastestCar();
         return cars.findAllTiedWith(fastestCar);
     }
